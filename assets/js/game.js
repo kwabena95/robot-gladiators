8f3a3c12a2529f11373dcffc27ed7fbb65ff838b
ebenezer <e.kankam95@hotmail.com>
2020-12-22 08:41:20 -0600

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,6 +2,7 @@
 const playerName = prompt("What is your robot's name?");
 let playerHealth = 100;
 let playerAttack = 10;
+let playerMoney = 10;
 
 // enemy info
 let enemyName = 'Roborto';
@@ -11,14 +12,43 @@ let enemyAttack = 12;
 const fight = () => {
     // Alert players that they are starting the round
     alert('Welcome to Robot Gladiators');
+    const promptFight = prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
-    /*Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable*/
-    enemyHealth -= playerAttack;
 
-    // Log a resulting message to the console so we know that it worked.
-    let enemyResult = `${playerName} attacked ${enemyName}. ${enemyName}, now has ${enemyHealth} health remaining.`;
 
-    console.log(enemyResult);
+    // if player choses to fight, then fight
+    if (promptFight === 'FIGHT' || promptFight === 'fight') {
+        /*Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable*/
+        enemyHealth -= playerAttack;
+
+        // Log a resulting message to the console so we know that it worked.
+        let enemyResult = `${playerName} attacked ${enemyName}. ${enemyName}, now has ${enemyHealth} health remaining.`;
+
+        console.log(enemyResult);
+
+    }// if player choses to skip
+    else if (promptFight === 'skip' || promptFight === 'SKIP') {
+        //confirm player wants to skip
+        const confirmSkip = confirm('Are you sure you want to quit?');
+        //if yes (true), leave fight
+        if (confirmSkip) {
+            alert(`${playerName} has decided to skip this fight. Goodbye!`);
+            playerName -= 2
+        } //if no (false), ask question again by running fight() again
+        else {
+            fight();
+        }
+    }
+    else {
+        alert('You need tp choose a valid option. Try again!');
+    }
+
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        alert(`${enemyName} has died!`);
+    } else {
+        alert(`${enemyName} still has ${enemyHealth} health left.`);
+    }
 
     /* Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.*/
     playerHealth -= enemyAttack;
@@ -28,19 +58,13 @@ const fight = () => {
 
     console.log(PlayerResult);
 
-    // check enemy's health
-    if (enemyHealth <= 0) {
-        alert(`${enemyName} has died!`);
-    } else {
-        alert(`${enemyName} still has ${enemyHealth} health left.`);
-    }
-
     // check player's health
     if (playerHealth <= 0) {
         alert(`${playerName} has died!`);
     } else {
         alert(`${playerName} still has ${playerHealth} health left.`);
     }
+
 
 }
 
