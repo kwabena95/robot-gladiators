7a3f035b9c8d88bc40b8c86a1039056b52903bd2
ebenezer <e.kankam95@hotmail.com>
2020-12-22 10:49:45 -0600

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,11 +11,25 @@ let enemyAttack = 12;
 
 const fight = (enemyName) => {
     // Alert players that they are starting the round
-    alert('Welcome to Robot Gladiators');
-    const promptFight = prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+    console.log('Welcome to Robot Gladiators');
 
-    // if player choses to fight, then fight
-    if (promptFight === 'FIGHT' || promptFight === 'fight') {
+    while (enemyHealth > 0 && playerHealth > 0) {
+        const promptFight = prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+        // if player choses to skip confirm and then stop the loop
+        if (promptFight === 'skip' || promptFight === 'SKIP') {
+            //confirm player wants to skip
+            const confirmSkip = confirm('Are you sure you want to quit?');
+            //if yes (true), leave fight
+            if (confirmSkip) {
+                console.log(`${playerName} has decided to skip this fight. Goodbye!`);
+
+                // subtract money from player money
+                playerMoney -= 10;
+                break;
+            }
+        }
+
         /*Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable*/
         enemyHealth -= playerAttack;
 
@@ -23,11 +37,14 @@ const fight = (enemyName) => {
         let enemyResult = `${playerName} attacked ${enemyName}. ${enemyName}, now has ${enemyHealth} health remaining.`;
 
         console.log(enemyResult);
+
         // check enemy's health
         if (enemyHealth <= 0) {
-            alert(`${enemyName} has died!`);
+            console.log(`${enemyName} has died!`);
+            playerMoney += 20;
+            break;
         } else {
-            alert(`${enemyName} still has ${enemyHealth} health left.`);
+            console.log(`${enemyName} still has ${enemyHealth} health left.`);
         }
 
         /* Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.*/
@@ -40,32 +57,41 @@ const fight = (enemyName) => {
 
         // check player's health
         if (playerHealth <= 0) {
-            alert(`${playerName} has died!`);
+            console.log(`${playerName} has died!`);
+            break;
         } else {
-            alert(`${playerName} still has ${playerHealth} health left.`);
+            console.log(`${playerName} still has ${playerHealth} health left.`);
         }
 
-
-    }// if player choses to skip
-    else if (promptFight === 'skip' || promptFight === 'SKIP') {
-        //confirm player wants to skip
-        const confirmSkip = confirm('Are you sure you want to quit?');
-        //if yes (true), leave fight
-        if (confirmSkip) {
-            alert(`${playerName} has decided to skip this fight. Goodbye!`);
-            playerName -= 2
-        } //if no (false), ask question again by running fight() again
-        else {
-            fight();
-        }
-    }
-    else {
-        alert('You need tp choose a valid option. Try again!');
     }
 
 }
 
+
 for (let i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+
+    // if player is still alive, keep fighting
+    if (playerHealth > 0) {
+
+        // let the player know what round they are in
+        console.log('Welcome to Robot Gladiators!' + (i + 1));
+
+        // pick new enemy from array
+        let pickedEnemyName = enemyNames[i];
+
+        // reset next enemy health
+        enemyHealth = 50;
+
+        // use debugger to pause script from running and check what's going on at that moment in the code
+        debugger;
+
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+
+    } else {
+        // alert user that they have lost.
+        console.log("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
 
